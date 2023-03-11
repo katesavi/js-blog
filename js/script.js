0b1cbe1fc7ccb19d816b801e8b47651c1c4339ab
Katia Savitska <katesavi@gmail.com>
2023-03-11 19:57:07 +0100

    css, js, package...modified

--- js/script.js
+++ js/script.js
@@ -1,16 +1,37 @@
 'use strict';
+console.log('test1');
+function titleClickHandler(event){
+}
+  console.log('Link was clicked!');
 
-function buttonClicked(buttonName){
-  console.log(buttonName + ' został kliknięty');
+  /* remove class 'active' from all article links  */
+
+  const activeLinks = document.querySelectorAll('.titles a.active');
+
+for(let activeLink of activeLinks){
+  activeLink.classList.remove('active');
+
 }
 
-const buttonTest = document.getElementById('button-test');
+  /* add class 'active' to the clicked link */
 
-buttonTest.addEventListener('click', function(){
-  buttonClicked('Test button');
-});
+  /* remove class 'active' from all articles */
+  const activeArticles = document.querySelectorAll(' .titles a.active');
+  console.log(activeArticles)
+
+for(let activeArticle of activeArticles){
+  activeLink.classList.remove('active');
+}
+
+  /* get 'href' attribute from the clicked link */
+
+  /* find the correct article using the selector (value of 'href' attribute) */
+
+  /* add class 'active' to the correct article */
 
 
+const links = document.querySelectorAll('.titles a');
 
-
-
+for(let link of links){
+  link.addEventListener('click', titleClickHandler);
+}
