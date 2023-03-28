c9bfa5611d0ae5f9ec6489e8dc7e67b3cd83998d
Katia Savitska <katesavi@gmail.com>
2023-03-28 19:09:32 +0200

    zadanie 5.3

--- js/script.js
+++ js/script.js
@@ -1,52 +1,39 @@
 'use strict';
-console.log('test1');
-function titleClickHandler(event){
-  const clickedElement = this;
-  for(let activeLink of activeLinks) { 
-    activeLink.classList.add('active');
-    event.preventDefault();
-  }
-  
-  
-``
-}
-  console.log('Link was clicked!');
-
-  
-
-  /* remove class 'active' from all article links  */
-
-  const activeLinks = document.querySelectorAll('.titles a.active');
-
-for(let activeLink of activeLinks){
-  activeLink.classList.remove('active');
-}
-console.log('clickedElement:', clickedElement);
-  /* add class 'active' to the clicked link */
-
-  /* remove class 'active' from all articles */
-  const activeArticles = document.querySelectorAll(' .titles a.active');
-  console.log(activeArticles)
-
-for(let activeArticle of activeArticles){
-  activeLink.classList.remove('active');
-}
-
-  /* get 'href' attribute from the clicked link */
-const articleSelector = clickedElement.getAttribute('#article-1')
-
-
-
-
-  /* find the correct article using the selector (value of 'href' attribute) */
-const targetArticle = document.querySelector('#article-2');
-console.log('article-2');
-  /* add class 'active' to the correct article */
-
-
 const links = document.querySelectorAll('.titles a');
+const articles = document.querySelectorAll('.post');
 
+// dodaje listener do wszystkich linków (aby uruchamiała się funkcja titleClickHandler po kliknięciu)
 for(let link of links){
   link.addEventListener('click', titleClickHandler);
 }
-console.log('articleSelector');
+
+function titleClickHandler(event){
+  // blokujemy domyślne zachowanie przeglądarki (zmiana adresu url)
+  event.preventDefault();
+
+  // usuń klasę active ze wszystkich linków na liście tytułów,
+  for(let activeLink of links){
+    activeLink.classList.remove('active');
+  }
+
+  // dodaj klasę active do klikniętego linka,
+  const clickedElement = this;
+  clickedElement.classList.add('active');
+
+  // ukryj wszystkie artykuły:
+  // usuń klasę active ze wszystkich artykułów,
+  for(let article of articles) {
+    article.classList.remove('active');
+  }
+
+  // znajdź artykuł do wyświetlenia:
+  // z klikniętego linka weź zawartość atrybutu href, np. #article-2,
+  const articleId = clickedElement.getAttribute('href')
+
+  // znajdź na stronie element pasujący do selektora takiego, jak wartość atrybutu href, np. #article-2 – czyli szukamy elementu o id="article-2",
+  const targetArticle = document.querySelector(articleId);
+
+  // wyświetl znaleziony artykuł:
+  // dodaj klasę active do znalezionego artykułu.
+  targetArticle.classList.add('active');
+}
